cce64a890fe798077d476b0c6593bd3210d7454f
Maciej Fec <maciejfec1996@gmail.com>
2022-04-19 14:36:34 +0100

    added validation for floats

--- codeTest.py
+++ codeTest.py
@@ -8,7 +8,8 @@ In a language of your choice, using all best practice principles you’re aware 
 My assumptions:
 - X cannot always be included, hence in some cases the final number will be less than, but as close to X as possible.
 - If A is 0, result is 0.
-- X cannot be less than A. 
+- X cannot be less than A.
+- A and/or X cannot be floats
 """
 
 def multiples_of_a_until_x(a, x):
@@ -19,7 +20,7 @@ def multiples_of_a_until_x(a, x):
     if a == 0:
         return 0
     
-    if x < a:
+    if x < a or not isinstance(a, int) or  not isinstance(x, int):
         # Returns -1 to indicate error if X is less than A
         return -1
     
@@ -31,9 +32,14 @@ def display_solution(a, x):
     then multiples of A + 2 until 3X
     """
     
-    for i in range(3):
-        """
-        Adds i to a and multiplies x by i + 1 three times 
-        to achieve the outcome of A until X, A + 1 until 2X, and A + 2 until 3X
-        """
-        print(multiples_of_a_until_x(a + i, x * (i + 1)))
+    result = multiples_of_a_until_x(a, x)
+
+    if result == -1:
+        print("A and/or X are invalid")
+    else:
+        for i in range(3):
+            """
+            Adds i to a and multiplies x by i + 1 three times 
+            to achieve the outcome of A until X, A + 1 until 2X, and A + 2 until 3X
+            """
+            print(multiples_of_a_until_x(a + i, x * (i + 1)))
